e35db6f70bff917a97b4f9b193701450038fc1f4
MrMrProgrammer <mr.mr.programmer@gmail.com>
2026-04-24 15:35:36 +0330

    adding rabbitmq

--- eshop/config/celery.py
+++ eshop/config/celery.py
@@ -1,20 +1,35 @@
 import os
 from celery import Celery
+from kombu import Exchange, Queue
 
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 app = Celery("config")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
+app.conf.task_queues = [
+    Queue(
+        "tasks",
+        Exchange("tasks"),
+        routing_key="tasks",
+        queue_arguments={"x-max-priority": 10}
+    )
+]
+
+app.conf.task_acks_late = True
+app.conf.task_default_priority = 5
+app.conf.worker_prefetch_multiplier = 1
+app.conf.concurrency = 1
+
 # queues => celery,celery:1,celery:2,celery:3
 
 # rate limit
 # app.conf.task_default_rate_limit = "1/m"
 
-app.conf.broker_transport_options = {
-    "priority_steps": list(range(10)),
-    "sep": ":",
-    "queue_order_strategy": "priority"
-}
+# app.conf.broker_transport_options = {
+#     "priority_steps": list(range(10)),
+#     "sep": ":",
+#     "queue_order_strategy": "priority"
+# }
 
 app.autodiscover_tasks()
