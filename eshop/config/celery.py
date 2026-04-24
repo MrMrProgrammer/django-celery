f922a739d8815b7c844c88230f29dfcff3bdbc5b
MrMrProgrammer <mr.mr.programmer@gmail.com>
2026-04-25 00:27:37 +0330

    تلاش برای اتصال به rabbitmq

--- eshop/config/celery.py
+++ eshop/config/celery.py
@@ -1,6 +1,7 @@
 import os
 from celery import Celery
 from kombu import Exchange, Queue
+import time
 
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
@@ -20,6 +21,42 @@ app.conf.task_acks_late = True
 app.conf.task_default_priority = 5
 app.conf.worker_prefetch_multiplier = 1
 app.conf.concurrency = 1
+
+
+@app.task(queue="tasks")
+def task_1():
+    time.sleep(3)
+    return
+
+
+@app.task(queue="tasks")
+def task_2():
+    time.sleep(3)
+    return
+
+
+@app.task(queue="tasks")
+def task_3():
+    time.sleep(3)
+    return
+
+
+@app.task(queue="tasks")
+def task_4():
+    time.sleep(3)
+    return
+
+
+def handle_tasks():
+    task_1.apply_async(priority=1)
+    task_1.apply_async(priority=1)
+    task_2.apply_async(priority=2)
+    task_2.apply_async(priority=2)
+    task_3.apply_async(priority=3)
+    task_3.apply_async(priority=3)
+    task_4.apply_async(priority=4)
+    task_4.apply_async(priority=4)
+
 
 # queues => celery,celery:1,celery:2,celery:3
 
--- eshop/config/settings.py
+++ eshop/config/settings.py
@@ -119,5 +119,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-CELERY_BROKER_URL = os.environ.get("CELERY_BROKER", "redis://redis:6379/0")
-CELERY_RESULT_BACKEND = os.environ.get("CELERY_BACKEND", "redis://redis:6379/0")
+# CELERY_BROKER_URL = os.environ.get("CELERY_BROKER", "redis://redis:6379/0")
+CELERY_BROKER_URL = os.environ.get(
+    "CELERY_BROKER",
+    "amqp://guest:guest@rabbitmq:5672"
+)
+CELERY_RESULT_BACKEND = os.environ.get(
+    "CELERY_BACKEND",
+    "redis://redis:6379/0"
+)
