68e211990eb84c259be16d0afe4fd87b9a3d36f4
MrMrProgrammer <mr.mr.programmer@gmail.com>
2026-04-24 12:12:53 +0330

    adding queue

--- eshop/config/celery.py
+++ eshop/config/celery.py
@@ -2,14 +2,16 @@ import os
 from celery import Celery
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 app = Celery("config")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
+# queues => celery,celery:1,celery:2,celery:3
 
-app.conf.task_routes = {
-    "notifications.tasks.send_discount_emails": {"queue": "queue1"},
-    "notifications.tasks.process_data_for_ml": {"queue": "queue2"}
+app.conf.broker_transport_options = {
+    "priority_steps": list(range(10)),
+    "sep": ":",
+    "queue_order_strategy": "priority"
 }
 
 app.autodiscover_tasks()
--- eshop/notifications/tasks.py
+++ eshop/notifications/tasks.py
@@ -1,11 +1,26 @@
 from celery import shared_task
+import time
 
 
 @shared_task
-def send_discount_emails():
-    pass
+def task_1(queue="celery"):
+    time.sleep(3)
+    return
 
 
 @shared_task
-def process_data_for_ml():
-    pass
+def task_2(queue="celery:1"):
+    time.sleep(3)
+    return
+
+
+@shared_task
+def task_3(queue="celery:2"):
+    time.sleep(3)
+    return
+
+
+@shared_task
+def task_4(queue="celery:3"):
+    time.sleep(3)
+    return
