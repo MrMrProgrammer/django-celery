14973df1e94443291e3f8f65a96b2886c2672052
MrMrProgrammer <mr.mr.programmer@gmail.com>
2026-04-26 23:11:34 +0330

    مثال برای تفاوت async و sync

--- eshop/notifications/tasks.py
+++ eshop/notifications/tasks.py
@@ -2,43 +2,49 @@ from celery import shared_task, group, chain
 import time
 
 
-@shared_task(rate_limit="1/m")
-def task_1(queue="celery"):
+# @shared_task(rate_limit="1/m")
+# def task_1(queue="celery"):
+#     time.sleep(3)
+#     return
+
+
+# @shared_task
+# def task_2(queue="celery:1"):
+#     time.sleep(3)
+#     return
+
+
+# @shared_task
+# def task_3(queue="celery:2"):
+#     time.sleep(3)
+#     return
+
+
+# @shared_task
+# def task_4(queue="celery:3"):
+#     time.sleep(3)
+#     return
+
+
+# group_tasks = group(
+#     task_1.s(),
+#     task_2.s(),
+#     task_3.s(),
+#     task_4.s()
+# )
+# group_tasks.apply_async()
+
+
+# chain_tasks = chain(
+#     task_1.s(),
+#     task_2.s(),
+#     task_3.s(),
+#     task_4.s()
+# )
+# chain_tasks.apply_async()
+
+
+@shared_task(queue="tasks")
+def send_sms_to_user():
     time.sleep(3)
-    return
-
-
-@shared_task
-def task_2(queue="celery:1"):
-    time.sleep(3)
-    return
-
-
-@shared_task
-def task_3(queue="celery:2"):
-    time.sleep(3)
-    return
-
-
-@shared_task
-def task_4(queue="celery:3"):
-    time.sleep(3)
-    return
-
-
-group_tasks = group(
-    task_1.s(),
-    task_2.s(),
-    task_3.s(),
-    task_4.s()
-)
-group_tasks.apply_async()
-
-
-chain_tasks = chain(
-    task_1.s(),
-    task_2.s(),
-    task_3.s(),
-    task_4.s()
-)
-chain_tasks.apply_async()
+    print("sms has been sent")
