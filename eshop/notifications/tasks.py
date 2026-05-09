9180b3b74a99e850da3d0df4d89865d086363a2c
MrMrProgrammer <mr.mr.programmer@gmail.com>
2026-05-09 22:40:58 +0330

    مدیریت خطا در celery

--- eshop/notifications/tasks.py
+++ eshop/notifications/tasks.py
@@ -1,5 +1,6 @@
 from celery import shared_task, group, chain
 import time
+import logging
 
 
 # @shared_task(rate_limit="1/m")
@@ -44,7 +45,17 @@ import time
 # chain_tasks.apply_async()
 
 
+# @shared_task(queue="tasks")
+# def send_sms_to_user():
+#     time.sleep(3)
+#     print("sms has been sent")
+
+
 @shared_task(queue="tasks")
-def send_sms_to_user():
-    time.sleep(3)
-    print("sms has been sent")
+def my_task():
+    try:
+        print("this is my task")
+        raise ValueError("value is not valid")
+    except Exception as e:
+        logging.error(f"an exception has been occurred. {e}")
+        raise ConnectionError("connection error occurred")
