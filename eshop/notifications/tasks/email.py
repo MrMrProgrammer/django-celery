9e5895c20f77aec170ce2ede8903e9d88f82e650
MrMrProgrammer <mr.mr.programmer@gmail.com>
2026-05-10 17:55:37 +0330

    استفاده از time out

--- eshop/notifications/tasks/email.py
+++ eshop/notifications/tasks/email.py
@@ -9,7 +9,18 @@ import time
 #     print("email has been sent to user")
 
 
-@app.task(queue="tasks", time_limit=5)
+@app.task(queue="tasks", time_limit=10)
 def send_email_to_user():
     time.sleep(6)
     return "Email has been sent to user successfully"
+
+
+def send_email():
+    result = send_email_to_user.delay()
+
+    try:
+        task_result = result.get(timeout=4)
+
+    except TimeoutError:
+        print("Task timed out")
+        # result.revoke(terminate=True)
