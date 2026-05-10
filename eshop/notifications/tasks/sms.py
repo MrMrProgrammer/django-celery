126a333df0da7d59906aa3d95b587f8065bdcb3e
MrMrProgrammer <mr.mr.programmer@gmail.com>
2026-05-10 07:50:02 +0330

    اجرای گروهی تسک ها و مدیریت خطای آنها

--- eshop/notifications/tasks/sms.py
+++ eshop/notifications/tasks/sms.py
@@ -1,12 +1,50 @@
 from config.celery import app
+from celery import group
 
 
-@app.task(
-    queue="tasks",
-    autoretry_for=(ConnectionError,),
-    default_retry_delay=5,
-    retry_kwargs={"max_retries": 5}
-)
-def send_sms_to_user():
-    raise ConnectionError("Connection Error ...")
-    # print("sms has been sent to user")
+# @app.task(
+#     queue="tasks",
+#     autoretry_for=(ConnectionError,),
+#     default_retry_delay=5,
+#     retry_kwargs={"max_retries": 5}
+# )
+# def send_sms_to_user():
+#     raise ConnectionError("Connection Error ...")
+#     # print("sms has been sent to user")
+
+
+phone_numbers = [
+    "09370522995",
+    "09125468466",
+    "09301843435",
+    "09038464656",
+    "09389646565",
+]
+
+
+@app.task(queue="tasks")
+def send_sms_to_user(phone_number: str):
+    if phone_number.startswith("0903"):
+        raise ValueError("Invalid phone number")
+
+    return f"Message has been sent to {phone_number}"
+
+
+def handle_result(result):
+    if result.successful():
+        print(f"Task complited: {result.get()}")
+
+    elif result.failed() and isinstance(result.result, ValueError):
+        print(f"Task failed: {result.result}")
+
+    elif result.status == "REVOKED":
+        print(f"Task was revoked: {result.id}")
+
+
+def run_tasks():
+    task_group = group(send_sms_to_user.s(number) for number in phone_numbers)
+    result_group = task_group.apply_async()
+    result_group.get(disable_sync_subtasks=False, propagate=False)
+
+    for result in result_group:
+        handle_result(result)
