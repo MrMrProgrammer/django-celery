441e2738cd2235d153205fee66eaf3a85e60cfa6
MrMrProgrammer <mr.mr.programmer@gmail.com>
2026-05-10 17:40:27 +0330

    ایجاد time limit برای انجام تسک ها

--- eshop/notifications/tasks/email.py
+++ eshop/notifications/tasks/email.py
@@ -1,7 +1,15 @@
-from celery import shared_task
+# from celery import shared_task
+from config.celery import app
+import time
 
 
-@shared_task(queue="tasks")
+# @shared_task(queue="tasks")
+# def send_email_to_user():
+#     raise ConnectionError("connection lost ...")
+#     print("email has been sent to user")
+
+
+@app.task(queue="tasks", time_limit=5)
 def send_email_to_user():
-    raise ConnectionError("connection lost ...")
-    print("email has been sent to user")
+    time.sleep(6)
+    return "Email has been sent to user successfully"
